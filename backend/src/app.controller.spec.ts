c95cb82544783d9bee04d4d6ab69e5d90c17eff6
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-09-21 10:42:53 +1200

    chore: modify test string to show passed tests

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World!, Hello CISE Team!, Let\'s build SPEED App! "', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
