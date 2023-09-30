84b51da1a6519aec52f40d990f1d294bb9717229
VincentNoc <kst0629@autuni.ac.nz>
2023-09-30 18:01:07 +1300

    welcome paragraph.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import "../styles/index.scss";
+
 export default function Home() {
   return (
     <div className="container">
-      <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+      <div className="container-within">
+        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <p>
+          Welcome to Speed, the number one place to analyse the validity of your
+          scholarly articles.
+        </p>
+      </div>
     </div>
   );
 }
