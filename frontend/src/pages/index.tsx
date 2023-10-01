a44f8136dda58029c81ec642db3281f99f5c2cc3
VincentNoc <vk6953@hotmail.com>
2023-10-02 01:22:12 +1300

    AUT student section

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,7 +13,23 @@ export default function Home() {
           <p>Create an account to submit your articles.</p>
           <button className="SignUp-btn">Sign Up</button>
         </div>
-        <div className="AUT-section"></div>
+        <div className="AUT-section">
+          <div className="container-within">
+            <h1>AUT Student?</h1>
+            <p>
+              If you're an AUT student looking for articlies to use in your
+              study, look no further then
+              <a
+                href="https://library.aut.ac.nz/"
+                className="AUT-link"
+                target="_blank"
+                rel="noopner noreferrer"
+              >
+                AUT Library.
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
