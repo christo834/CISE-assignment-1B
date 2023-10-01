3ebaeee60a5f3b0df4806b8d18d2230fe1b5392c
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-10-02 01:19:26 +1300

    feat: introduce crud directories and schemas
    installed mongoose dependepcy
    did % nest g res posts
    did % touch posts.schema.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://danielllip:<danielllip>@cluster0.yfilps9.mongodb.net/?retryWrites=true&w=majority'), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
