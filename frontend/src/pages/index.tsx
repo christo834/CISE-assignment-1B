504bb9d58e6b870f0e2887dcd7dfd73b693adce9
VincentNoc <kst0629@autuni.ac.nz>
2023-10-02 00:51:59 +1300

    putting more content

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,15 +2,18 @@ import "../styles/index.scss";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="container-within">
-        <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-        <p>
-          Welcome to Speed, the number one place to analyse the validity of your
-          scholarly articles.
-        </p>
-        <p>Create an account to submit your articles.</p>
-        <button className="SignUp-btn">Sign Up</button>
+    <div>
+      <div className="container">
+        <div className="container-within">
+          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+          <p>
+            Welcome to Speed, the number one place to analyse the validity of
+            your scholarly articles.
+          </p>
+          <p>Create an account to submit your articles.</p>
+          <button className="SignUp-btn">Sign Up</button>
+        </div>
+        <div className="AUT-section"></div>
       </div>
     </div>
   );
