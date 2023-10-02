8316a32b1dac7ec666b3be44a861acefd702b146
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-10-02 13:37:42 +1300

    fix: bug on not connecting to mongo db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleModule } from './articles/article.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+const OLD_PASSWORD = process.env.OLD_PASSWORD;
 
 @Module({
   imports: [
-    ArticleModule, MongooseModule.forRoot('mongodb+srv://user1:wO7OY6V0vzwUNEAn@cluster0.yfilps9.mongodb.net/?retryWrites=true&w=majority')
+    ArticleModule, MongooseModule.forRoot(`mongodb+srv://user1:wO7OY6V0vzwUNEAn@cluster0.yfilps9.mongodb.net/?retryWrites=true&w=majority`)
   ],
   controllers: [AppController],
   providers: [AppService], 
