3b926224a50278b98645482fcfade3cbb8ea5204
VincentNoc <kst0629@autuni.ac.nz>
2023-10-02 20:42:05 +1300

    device responsiveness

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -33,6 +33,28 @@ export default function Home() {
             <div className="home-guideline-design">
               <h1>REFERENCING AND APA</h1>
             </div>
+            <h2 className="refer-link">Referencing guides</h2>
+            <a
+              href="https://aut.ac.nz.libguides.com/APA7th"
+              target="_blank"
+              className="refer-link"
+            >
+              &gt; APA 7th referencing guide
+            </a>
+            <a
+              href="https://aut.ac.nz.libguides.com/turabian"
+              target="_blank"
+              className="refer-link"
+            >
+              &gt; Chicago referencing guide
+            </a>
+            <a
+              href="https://aut.ac.nz.libguides.com/Harvard"
+              target="_blank"
+              className="refer-link"
+            >
+              &gt; Harvard referencing guide
+            </a>
           </div>
         </div>
       </div>
