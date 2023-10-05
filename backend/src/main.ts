b9922d8981eac52774af4b744ae422525a069b7f
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-10-05 21:42:13 +1300

    feat: update main.ts cors parameter
    add front end vercel link into backend directory main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://vercel.com/gxc1998github/cise-frontend/2N5nYhsb2WUsD4r35duq9RseQuQn', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+
+
   dotenv.config();
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
