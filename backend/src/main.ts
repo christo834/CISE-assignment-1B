c8c8ed2a2da4ce02bb84d80fb8560baeb381261f
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-10-06 01:44:53 +1300

    test:  make src change from api to article

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,7 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://cise-frontend-h8i1dzse9-gxc1998github.vercel.app', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-
+  app.enableCors();
 
   dotenv.config();
   const PORT = process.env.PORT || 8000;
