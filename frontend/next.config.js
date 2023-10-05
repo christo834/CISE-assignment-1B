f1b0f1387a945bbeb3709f972c329726e34fe1a6
Christopher Li <84425223+christo834@users.noreply.github.com>
2023-10-05 22:12:27 +1300

    Revert "fix: config.js"
    This reverts commit 228b597c3a978792f6d659a3435d1c8d0b60aacc.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://cise-assignment-1-b-qnho.vercel.app/api/:path*',
-      },
-    ]
-  },
-};
+const nextConfig = {};
 
+module.exports = nextConfig;
