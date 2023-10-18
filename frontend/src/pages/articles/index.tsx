0d010d1fc2c935b6f9dee951c1153cd3e4db6c7e
VincentNoc <kst0629@autuni.ac.nz>
2023-10-19 11:40:20 +1300

    titles
    made titles for the different sections in the articles.

--- frontend/src/pages/articles/index.tsx
+++ frontend/src/pages/articles/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import "../../styles/table.css"
+import { useEffect, useState } from "react";
+import "../../styles/table.css";
 
 interface Article {
   _id: string;
@@ -8,17 +8,17 @@ interface Article {
   source: string;
   year: number;
   doi: string;
-  claim: string
+  claim: string;
   evidence_level: string;
   summary: string;
 }
 
 const Articles = () => {
   const [articles, setArticles] = useState<Article[]>([]);
-  //searches for true articles 
+  //searches for true articles
   useEffect(() => {
     const fetchArticles = async () => {
-      const response = await fetch('http://localhost:8000/article/analysed');
+      const response = await fetch("http://localhost:8000/article/analysed");
       const data = await response.json();
 
       setArticles(data.articles);
@@ -30,15 +30,33 @@ const Articles = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-white">
       {articles.map((article) => (
-        <div className="rounded overflow-hidden shadow-lg p-4 text-white border-white border-2 text-wrap" key={article._id}>
+        <div
+          className="rounded overflow-hidden shadow-lg p-4 text-white border-white border-2 text-wrap"
+          key={article._id}
+        >
           <h3 className="font-bold text-xl mb-2">{article.title}</h3>
-          <p><b>Authors: </b> {article.authors.join(', ')}</p>
-          <p><b>Source: </b>{article.source}</p>
-          <p><b>Year: </b> {article.year}</p>
-          <p><b>DOI: </b> {article.doi}</p>
-          <p><b>Claim: </b> {article.claim}</p>
-          <p><b>Evidence Level: </b> {article.evidence_level}</p>
-          <p><b>Summary: </b> {article.summary}</p>
+          <p>
+            <b>Authors: </b> {article.authors.join(", ")}
+          </p>
+          <p>
+            <b>Source: </b>
+            {article.source}
+          </p>
+          <p>
+            <b>Year: </b> {article.year}
+          </p>
+          <p>
+            <b>DOI: </b> {article.doi}
+          </p>
+          <p>
+            <b>Claim: </b> {article.claim}
+          </p>
+          <p>
+            <b>Evidence Level: </b> {article.evidence_level}
+          </p>
+          <p>
+            <b>Summary: </b> {article.summary}
+          </p>
         </div>
       ))}
     </div>
--- frontend/src/pages/moderator/index.tsx
+++ frontend/src/pages/moderator/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import "../../styles/table.css"
-import swal from 'sweetalert';
-import { SpinnerInfinity } from 'spinners-react';
+import { useEffect, useState } from "react";
+import "../../styles/table.css";
+import swal from "sweetalert";
+import { SpinnerInfinity } from "spinners-react";
 
 interface Moderator {
   _id: string;
@@ -18,15 +18,16 @@ const Moderator = () => {
   const [articles, setArticles] = useState<Moderator[]>([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     const fetchArticles = async () => {
       try {
         setLoading(true);
 
-        const response = await fetch('https://cise-backend-5103.vercel.app/article/all');
+        const response = await fetch(
+          "https://cise-backend-5103.vercel.app/article/all"
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch');
+          throw new Error("Failed to fetch");
         }
 
         const data = await response.json();
@@ -36,27 +37,30 @@ const Moderator = () => {
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchArticles();
   }, []);
   const handleSetModeratorTrue = async (id: string) => {
-    const article = articles.find(article => article._id === id);
-    if (article && article.moderated === 'true') {
+    const article = articles.find((article) => article._id === id);
+    if (article && article.moderated === "true") {
       swal("Info", "This article has already been approved", "info");
       return;
     }
 
-    const response = await fetch(`https://cise-backend-5103.vercel.app/article/${id}/true`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      `https://cise-backend-5103.vercel.app/article/${id}/true`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (response.ok) {
-      const updatedArticles = articles.map(article =>
-        article._id === id ? { ...article, moderated: 'true' } : article
+      const updatedArticles = articles.map((article) =>
+        article._id === id ? { ...article, moderated: "true" } : article
       );
       setArticles(updatedArticles);
       swal("Success", "Article approved successfully", "success");
@@ -66,22 +70,25 @@ const Moderator = () => {
   };
 
   const handleSetModeratorFalse = async (id: string) => {
-    const article = articles.find(article => article._id === id);
-    if (article && article.moderated === 'false') {
+    const article = articles.find((article) => article._id === id);
+    if (article && article.moderated === "false") {
       swal("Info", "This article has already been disapproved", "info");
       return;
     }
 
-    const response = await fetch(`https://cise-backend-5103.vercel.app/article/${id}/false`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      `https://cise-backend-5103.vercel.app/article/${id}/false`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (response.ok) {
-      const updatedArticles = articles.map(article =>
-        article._id === id ? { ...article, moderated: 'false' } : article
+      const updatedArticles = articles.map((article) =>
+        article._id === id ? { ...article, moderated: "false" } : article
       );
       setArticles(updatedArticles);
       swal("Success", "Article disapproved successfully", "success");
@@ -93,25 +100,54 @@ const Moderator = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-white">
       {loading ? (
-          <div className="fixed inset-0 flex items-center justify-center">
-          <SpinnerInfinity size={200} thickness={180} speed={180} color="rgba(71, 172, 57, 1)" secondaryColor="rgba(57, 172, 151, 0.44)" />
-        </div>  
+        <div className="fixed inset-0 flex items-center justify-center">
+          <SpinnerInfinity
+            size={200}
+            thickness={180}
+            speed={180}
+            color="rgba(71, 172, 57, 1)"
+            secondaryColor="rgba(57, 172, 151, 0.44)"
+          />
+        </div>
       ) : (
         articles.map((article) => (
-          <div className="rounded overflow-hidden shadow-lg p-4 text-white border-white border-2 text-wrap my-4 " key={article._id}>
-            <h3 className="font-bold text-xl mb-2 text-wrap mb-6">{article.title}</h3>
-            <p>{article.authors.join(', ')}</p>
-            <p>{article.source}</p>
-            <p>{article.year}</p>
-            <p className='italic'>{article.doi}</p>
-            <p>{article.summary}</p>
+          <div
+            className="rounded overflow-hidden shadow-lg p-4 text-white border-white border-2 text-wrap my-4 "
+            key={article._id}
+          >
+            <h3 className="font-bold text-xl mb-2 text-wrap mb-6">
+              {article.title}
+            </h3>
+            <p>
+              <div className="text-yellow-500">Author:</div>{" "}
+              {article.authors.join(", ")}
+            </p>
+            <p>
+              <div className="text-yellow-500">Source:</div> {article.source}
+            </p>
+            <p>
+              <div className="text-yellow-500">Year:</div> {article.year}
+            </p>
+            <p className="italic">
+              <div className="text-yellow-500">DOI:</div>
+              {article.doi}
+            </p>
+            <p>
+              <div className="text-yellow-500">Summary:</div> {article.summary}
+            </p>
 
             <div className="flex justify-between mt-4">
-              <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded" onClick={() => handleSetModeratorTrue(article._id)}>
+              <button
+                className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handleSetModeratorTrue(article._id)}
+              >
                 Approve
               </button>
-              <button className="bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded"  onClick={() => handleSetModeratorFalse(article._id)}>
-                Disapprove 
+              <button
+                className="bg-red-700 hover:bg-red-900 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handleSetModeratorFalse(article._id)}
+              >
+                Disapprove
               </button>
             </div>
           </div>
